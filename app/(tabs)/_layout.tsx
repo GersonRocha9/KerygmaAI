d8ac1b8909218ebe26d1f6d5b65b65af741b1e74
Gerson Rocha <gersonrocha9@gmail.com>
2025-10-24 10:14:29 -0300

    refactor: update tab icons and integrate theme colors for improved UI consistency

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,19 @@
+import { useThemeColors } from "@/src/hooks/useTheme";
 import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 
 import React from "react";
 
 export default function TabLayout() {
+  const colors = useThemeColors();
+
   return (
-    <NativeTabs>
+    <NativeTabs iconColor={colors.primary}>
       <NativeTabs.Trigger name="index">
         <Label>Início</Label>
-        <Icon sf="house.fill" drawable="custom_android_drawable" />
+        <Icon sf="house" drawable="custom_android_drawable" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="devotional">
-        <Icon sf="book.pages" drawable="custom_settings_drawable" />
+        <Icon sf="books.vertical" drawable="custom_settings_drawable" />
         <Label>Devocional</Label>
       </NativeTabs.Trigger>
     </NativeTabs>
